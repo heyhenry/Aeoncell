2a043d53feb387141df1f9e6df1ee0f0ed9748f6
heyhenry <henryyy3@hotmail.com>
2025-04-21 08:42:17 +0800

    Updated the parent classes' grid/pack usage. Updated layout for the registerpage.

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import customtkinter as ctk
 
 class Windows(ctk.CTk):
     def __init__(self, *args, **kwargs):
-        ctk.CTk.__init__(self, *args, **kwargs)
-        self.wm_title = ('Aeoncell')
+        super().__init__(*args, **kwargs)
+        self.title("Aeoncell")
+        self.geometry("800x600")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        container = ctk.CTkFrame(self, height=400, width=600)
-        container.pack(side='top', fill='both', expand=True)
+        container = ctk.CTkFrame(self)
+        container.grid(row=0, column=0, sticky="nswe")
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -14,30 +17,63 @@ class Windows(ctk.CTk):
         for P in (RegisterPage, LoginPage, DashboardPage, StatsPage, EntryPage, UpdateEntryPage):
             page = P(container, self)
             self.pages[P] = page
-            page.grid(row=0, column=0, sticky='nswe')
+            page.grid(row=0, column=0, sticky="nswe")
 
         self.show_page(RegisterPage)
 
     def show_page(self, selected_page):
-        page = self.pages[selected_page]
-        page.tkraise()
+        self.pages[selected_page].tkraise()
 
 class RegisterPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='Register Page')
-        btn = ctk.CTkButton(self, text='Go to Login', command=lambda: self.controller.show_page(LoginPage))
+        self.controller = controller
+        self.password_var = ctk.StringVar()
+        self.create_widgets()
 
-        lbl.pack()
-        btn.pack()
-        
+    def create_widgets(self):
+        page_title = ctk.CTkLabel(self, text="Register")
+        password = ctk.CTkLabel(self, text="Create Password:")
+        self.password_entry = ctk.CTkEntry(self, textvariable=self.password_var)
+        self.error_message = ctk.CTkLabel(self, text="")
+        submit = ctk.CTkButton(self, text="Register", command=self.process_password)
+
+        # horizontal centering
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=1)
+
+        # vertical centering
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+
+        # place widgets in the center column (col=1), and rows 1–5
+        page_title.grid(row=1, column=1, pady=10)
+        password.grid(row=2, column=1)
+        self.password_entry.grid(row=3, column=1)
+        self.error_message.grid(row=4, column=1)
+        submit.grid(row=5, column=1)
+
+    def process_password(self):
+        # validate password
+        password = self.password_var.get()
+        if len(password) < 8:
+            self.display_error("Password too short. 8 characters minium.")
+        elif password.isspace():
+            self.display_error("Whitespaces aren't allowed!")
+
+    def display_error(self, error_msg):
+        self.error_message.config(text=error_msg)
+        self.error_message.after(1000, lambda: self.error_message.config(text=""))
+        self.error_message.after(1000, self.password_var.set(""))
+
+
 class LoginPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='Login Page')
-        btn = ctk.CTkButton(self, text='Go to Dashboard', command=lambda: self.controller.show_page(DashboardPage))
+        lbl = ctk.CTkLabel(self, text="Login Page")
+        btn = ctk.CTkButton(self, text="Go to Dashboard", command=lambda: self.controller.show_page(DashboardPage))
 
         lbl.pack()
         btn.pack()
@@ -46,8 +82,8 @@ class DashboardPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='Dashboard Page')
-        btn = ctk.CTkButton(self, text='Go to Stats', command=lambda: self.controller.show_page(StatsPage))
+        lbl = ctk.CTkLabel(self, text="Dashboard Page")
+        btn = ctk.CTkButton(self, text="Go to Stats", command=lambda: self.controller.show_page(StatsPage))
 
         lbl.pack()
         btn.pack()
@@ -56,8 +92,8 @@ class StatsPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='Stats Page')
-        btn = ctk.CTkButton(self, text='Go to Entry', command=lambda: self.controller.show_page(EntryPage))
+        lbl = ctk.CTkLabel(self, text="Stats Page")
+        btn = ctk.CTkButton(self, text="Go to Entry", command=lambda: self.controller.show_page(EntryPage))
 
         lbl.pack()
         btn.pack()
@@ -66,8 +102,8 @@ class EntryPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='Entry Page')
-        btn = ctk.CTkButton(self, text='Go to UpdateEntry', command=lambda: self.controller.show_page(UpdateEntryPage))
+        lbl = ctk.CTkLabel(self, text="Entry Page")
+        btn = ctk.CTkButton(self, text="Go to UpdateEntry", command=lambda: self.controller.show_page(UpdateEntryPage))
 
         lbl.pack()
         btn.pack()
@@ -76,12 +112,12 @@ class UpdateEntryPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         self.controller = controller
         ctk.CTkFrame.__init__(self, parent)
-        lbl = ctk.CTkLabel(self, text='UpdateEntry Page')
-        btn = ctk.CTkButton(self, text='Go to Register', command=lambda: self.controller.show_page(RegisterPage))
+        lbl = ctk.CTkLabel(self, text="UpdateEntry Page")
+        btn = ctk.CTkButton(self, text="Go to Register", command=lambda: self.controller.show_page(RegisterPage))
 
         lbl.pack()
         btn.pack()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Windows()
     app.mainloop()
