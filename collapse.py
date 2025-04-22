9714b62716262b576b285bb74bb9575aa9f3ab24
heyhenry <henryyy3@hotmail.com>
2025-04-22 17:03:39 +0800

    Implemented better collapsibility behaviour in testing ground (collapsible) for future implementation to Aeoncell.

--- collapse.py
+++ collapse.py
@@ -1,4 +1,8 @@
 import customtkinter as ctk
+import sqlite3
+
+conn = sqlite3.connect("exercise_logs.db")
+cursor = conn.cursor()
 
 root = ctk.CTk()
 
@@ -7,20 +11,32 @@ ctk.set_default_color_theme("green")
 
 root.title("Collapse Me!")
 root.geometry("800x600")
-
+lst = []
 def toggle_frame():
     if latest_exercise_checkbox.get() == False:
-        exercise_frame.grid(row=3, column=1)
+        for i in lst:
+            i.grid_forget()
+        lst.clear()
+        cursor.execute("SELECT exercise_name, date, type FROM exercise_entries ORDER BY id DESC LIMIT 1")
+        entry = cursor.fetchone()
+        latest_entry.configure(exercise_frame, text=f"{entry[0]} | {entry[1]} | {entry[2]}", font=("", 24))
+        latest_entry.grid(row=0, column=0, sticky="w")
     else:
-        exercise_frame.grid_forget()
+        latest_entry.grid_forget()
+        cursor.execute("SELECT exercise_name, date, type FROM exercise_entries ORDER BY id DESC")
+        for i, entry in enumerate(cursor.fetchall()):  
+            lst.append(ctk.CTkLabel(exercise_frame, text=f"{entry[0]} | {entry[1]} | {entry[2]}", font=("", 24)))
+            lst[i].grid(row=i, column=0, sticky="w")
 
 title = ctk.CTkLabel(root, text="Recent Log", font=("", 32))
-latest_exercise_checkbox = ctk.CTkCheckBox(root, text="Poop", font=("", 32), command=toggle_frame)
-exercise_frame = ctk.CTkScrollableFrame(root, width=300, height=200)
-lst = []
-for i in range(10):
-    lst.append(ctk.CTkLabel(exercise_frame, text=f"Index: {i} something new"))
-    lst[i].grid(row=i, column=0)
+latest_exercise_checkbox = ctk.CTkCheckBox(root, text="Expand Log", font=("", 32), command=toggle_frame)
+exercise_frame = ctk.CTkScrollableFrame(root, width=500, height=200)
+latest_entry = ctk.CTkLabel(exercise_frame)
+cursor.execute("SELECT exercise_name, date, type FROM exercise_entries ORDER BY id DESC LIMIT 1")
+entry = cursor.fetchone()
+latest_entry.configure(exercise_frame, text=f"{entry[0]} | {entry[1]} | {entry[2]}", font=("", 24))
+latest_entry.grid(row=0, column=0, sticky="w")
+
 
 title.grid(row=1, column=1, padx=10, pady=10)
 latest_exercise_checkbox.grid(row=2, column=1)
