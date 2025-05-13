e6a15ea9d36ec572a33ffe0c587e28d7c3e06cc9
heyhenry <henryyy3@hotmail.com>
2025-05-13 13:41:06 +0800

    Implemented database creation methods for exercise_entries and steps_tracker

--- authmanager.py
+++ authmanager.py
@@ -8,6 +8,9 @@ class AuthManager:
         self.auth_connection = sqlite3.connect(self.auth_db)
         self.auth_cursor = self.auth_connection.cursor()
         self.create_auth_database()
+        self.create_exercise_database()
+        self.create_steps_database()
+        self.auth_connection.commit()
 
     def create_auth_database(self):
         self.auth_cursor.execute("CREATE TABLE IF NOT EXISTS authentication (desc TEXT, hash TEXT)")
@@ -32,3 +35,9 @@ class AuthManager:
         self.auth_cursor.execute("SELECT hash FROM authentication WHERE rowid = 1")
         if self.auth_cursor.fetchone():
             return True
+        
+    def create_exercise_database(self):
+        self.auth_cursor.execute("CREATE TABLE IF NOT EXISTS exercise_entries (id INTEGER, type TEXT, label TEXT, date TEXT, time TEXT, exercise_name TEXT, sets INTEGER, reps INTEGER, weight TEXT)")
+
+    def create_steps_database(self):
+        self.auth_cursor.execute("CREATE TABLE IF NOT EXISTS steps_tracker (id INTEGER, date TEXT, total_steps INTEGER)")
