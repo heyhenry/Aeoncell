e3ab29f5680b5fd55b4645dd6b4f3ce3d5ed4b1a
heyhenry <henryyy3@hotmail.com>
2025-06-03 23:20:31 +0800

    Database: Implemented initial row creation for profile_details.

--- database_manager.py
+++ database_manager.py
@@ -21,8 +21,8 @@ class DatabaseManager:
     def create_username_and_password(self, username, password):
         hashed_password = self.ph.hash(password)
         self.db_cursor.execute("INSERT INTO authentication (username, hashed_password) values (?, ?)", (username, hashed_password))
-        # create the initial profile details and store the given username
-        self.db_cursor.execute("INSERT INTO profile_details (username) values (?)", (username,))
+        # update the profile with user given username upon registration
+        self.db_cursor.execute("UPDATE profile_details SET username = ? WHERE rowid=1", (username,))
         self.db_connection.commit()
 
     def verify_password(self, given_password):
@@ -94,7 +94,7 @@ class DatabaseManager:
             first_name TEXT,
             last_name TEXT,
             age INTEGER,
-            height INTEGER
+            height INTEGER,
             current_weight INTEGER,
             goal_weight INTEGER,
             daily_sleep_goal FLOAT,
@@ -107,4 +107,9 @@ class DatabaseManager:
         )
         """
         self.db_cursor.execute(create_profile_table_query)
-
+        create_initial_row_query = """
+        INSERT INTO profile_details (username)
+        SELECT 'default_user'
+        WHERE NOT EXISTS (SELECT 1 FROM profile_details);
+        """
+        self.db_cursor.execute(create_initial_row_query)
