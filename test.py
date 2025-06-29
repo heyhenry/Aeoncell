7dfb4dc616393c127652e981a52376c922a1399e
heyhenry <henryyy3@hotmail.com>
2025-06-29 11:56:02 +0800

    Test: Added a new api test case.

--- test.py
+++ test.py
@@ -2,12 +2,21 @@ import requests
 
 # note to self, always lat before long
 
+# api from ipinfo.io
 resp = requests.get("https://ipinfo.io/json")
 loc = resp.json()["loc"]
 print(f"Ipinfo: {loc}")
 
+# api from ip-api.com
 resp = requests.get("http://ip-api.com/json")
 data = resp.json()
 latitude = data["lat"]
 longitude = data["lon"]
-print(f"ip-api: Lat: {latitude}, Lon: {longitude}")
+print(f"ip-api: {latitude}, {longitude}")
+
+# api from ipapi.co
+resp = requests.get("https://ipapi.co/json")
+data = resp.json()
+papi_latitude = data["latitude"]
+papi_longitude = data["longitude"]
+print(f"ipapi: {papi_latitude}, {papi_longitude}")
