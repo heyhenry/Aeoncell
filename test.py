15dd49c16d9e2a1a0a70f3d555747284a208795f
heyhenry <henryyy3@hotmail.com>
2025-06-29 16:31:48 +0800

    Test: Tested api fetch for weather and ip address. (Ensured functionality).

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 import requests
 
-# note to self, always lat before long
-
-# api from ipinfo.io
-resp = requests.get("https://ipinfo.io/json")
-loc = resp.json()["loc"]
-print(f"Ipinfo: {loc}")
-
-# api from ip-api.com
-resp = requests.get("http://ip-api.com/json")
+# api from geojs.io
+resp = requests.get("https://get.geojs.io/v1/ip/geo.json")
 data = resp.json()
-latitude = data["lat"]
-longitude = data["lon"]
-print(f"ip-api: {latitude}, {longitude}")
+latitude = data["latitude"]
+longitude = data["longitude"]
+print(f"geojs.io: {latitude}, {longitude}")
 
-# api from ipapi.co
-resp = requests.get("https://ipapi.co/json")
-data = resp.json()
-papi_latitude = data["latitude"]
-papi_longitude = data["longitude"]
-print(f"ipapi: {papi_latitude}, {papi_longitude}")
+response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,precipitation,weather_code&timezone=auto")
+data = response.json()
+current_units = data["current_units"]
+current_values = data["current"]
+
+timezone = f"Location: {data["timezone"]}"
+last_updated_time = f" Last Updated: {current_values["time"][11:]}"
+temperature = f"Temp: {current_values["temperature_2m"]} {current_units["temperature_2m"]}"
+precipitation = f"Precip: {current_values["precipitation"]} {current_units["precipitation"]}"
+weather_code = f"WMO: {current_values["weather_code"]}"
+
+print(timezone)
+print(last_updated_time)
+print(temperature)
+print(precipitation)
+print(weather_code)
