8de8f7d8382667fbca51c6ac95c1016e1bf8af1f
heyhenry <henryyy3@hotmail.com>
2025-07-16 10:13:41 +0800

    Removed redundant future annotation.

--- pages/session_entry_page.py
+++ pages/session_entry_page.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 import customtkinter as ctk
 from pages import BaseEntryPage
 
--- pages/single_entry_page.py
+++ pages/single_entry_page.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from pages import BaseEntryPage
 
 class SingleEntryPage(BaseEntryPage):
