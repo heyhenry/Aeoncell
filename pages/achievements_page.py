99049d77c6fe4a5e3a7b5f2d92d242ac3b10458d
heyhenry <henryyy3@hotmail.com>
2025-07-17 15:04:30 +0800

    Achievements: Implemented frames for each achievement and overall view.

--- pages/achievements_page.py
+++ pages/achievements_page.py
@@ -32,11 +32,55 @@ class AchievementsPage(ctk.CTkFrame):
         #region [Parent Frames]
         page_title = ctk.CTkLabel(content, text="Achievements", font=("", 24))
         page_message = ctk.CTkLabel(content, text="View your achievements here", font=("", 14))
-        self.achievements_section = ctk.CTkFrame(content, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=5, corner_radius=40, width=1200, height=800)
+        achievements_section = ctk.CTkFrame(content, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=5, corner_radius=40, width=1200, height=2600)
 
-        self.achievements_section.grid_propagate(False)
+        achievements_section.grid_propagate(False)
 
         page_title.grid(row=1, column=1, pady=(30, 0), sticky="w", padx=(0, 1000))
         page_message.grid(row=2, column=1, pady=(0, 50), sticky="w")
-        self.achievements_section.grid(row=3, column=1, pady=(0, 50))
+        achievements_section.grid(row=3, column=1, pady=(0, 50))
+
+        achievements_section.grid_rowconfigure(0, weight=1)
+        achievements_section.grid_rowconfigure(17, weight=1)
+
+        achievements_section.grid_columnconfigure(0, weight=1)
+        achievements_section.grid_columnconfigure(2, weight=1)
+
+        #endregion
+
+        #region [Sub Frames]
+        achievements_overview_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=5, corner_radius=0, width=1100, height=180)
+        achievement_slot_1_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_2_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_3_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_4_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_5_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_6_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_7_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_8_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_9_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_10_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_11_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_12_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_13_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_14_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+        achievement_slot_15_section = ctk.CTkFrame(achievements_section, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=3, corner_radius=0, width=1100, height=120)
+
+        achievements_overview_section.grid(row=1, column=1, pady=(30, 20))
+        achievement_slot_1_section.grid(row=2, column=1, pady=(30, 0))
+        achievement_slot_2_section.grid(row=3, column=1, pady=(30, 0))
+        achievement_slot_3_section.grid(row=4, column=1, pady=(30, 0))
+        achievement_slot_4_section.grid(row=5, column=1, pady=(30, 0))
+        achievement_slot_5_section.grid(row=6, column=1, pady=(30, 0))
+        achievement_slot_6_section.grid(row=7, column=1, pady=(30, 0))
+        achievement_slot_7_section.grid(row=8, column=1, pady=(30, 0))
+        achievement_slot_8_section.grid(row=9, column=1, pady=(30, 0))
+        achievement_slot_9_section.grid(row=10, column=1, pady=(30, 0))
+        achievement_slot_10_section.grid(row=11, column=1, pady=(30, 0))
+        achievement_slot_11_section.grid(row=12, column=1, pady=(30, 0))
+        achievement_slot_12_section.grid(row=13, column=1, pady=(30, 0))
+        achievement_slot_13_section.grid(row=14, column=1, pady=(30, 0))
+        achievement_slot_14_section.grid(row=15, column=1, pady=(30, 0))
+        achievement_slot_15_section.grid(row=16, column=1, pady=(30, 0))
+        
         #endregion
