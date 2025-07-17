e6250e233068180d25a572cf250fd146a02030e4
heyhenry <henryyy3@hotmail.com>
2025-07-17 09:38:11 +0800

    Achievements: Implemented initial frames.

--- pages/achievements_page.py
+++ pages/achievements_page.py
@@ -14,11 +14,29 @@ class AchievementsPage(ctk.CTkFrame):
         self.create_widgets()
 
     def create_widgets(self):
+        #region [Main Frames]
         navbar = Navbar(self, self.controller)
-        content = ctk.CTkFrame(self, fg_color="black", corner_radius=0)
+        content = ctk.CTkScrollableFrame(self, corner_radius=0)
 
         navbar.grid(row=0, column=0, sticky="nswe")
         content.grid(row=0, column=1, sticky="nswe")
 
-        dashboard_title = ctk.CTkLabel(content, text="This is the Achievements Page!", text_color="green")
-        dashboard_title.pack()
+        content.grid_rowconfigure(0, weight=1)
+        content.grid_rowconfigure(4, weight=1)
+
+        content.grid_columnconfigure(0, weight=1)
+        content.grid_columnconfigure(2, weight=1)
+
+        #endregion
+
+        #region [Parent Frames]
+        page_title = ctk.CTkLabel(content, text="Achievements", font=("", 24))
+        page_message = ctk.CTkLabel(content, text="View your achievements here", font=("", 14))
+        self.achievements_section = ctk.CTkFrame(content, fg_color=("#F5F0FF", "#2A1A4A"), border_color=("#B19CD9", "#9370DB"), border_width=5, corner_radius=40, width=1200, height=800)
+
+        self.achievements_section.grid_propagate(False)
+
+        page_title.grid(row=1, column=1, pady=(30, 0), sticky="w", padx=(0, 1000))
+        page_message.grid(row=2, column=1, pady=(0, 50), sticky="w")
+        self.achievements_section.grid(row=3, column=1, pady=(0, 50))
+        #endregion
