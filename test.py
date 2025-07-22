1bc2580f03a56e7a6502a1d7c3f55164b04046fb
heyhenry <henryyy3@hotmail.com>
2025-07-22 10:49:24 +0800

    Test: Learning more about getattr and using it to loop through achievements vars..

--- test.py
+++ test.py
@@ -1,24 +1,12 @@
-class MrRoboto:
-    def __init__(self,
-                    height: int = 100,
-                    weight: int = 1000,
-                    colour: str = "White"):
+class Person():
+    def __init__(self, gender, age, height, name):
+        self.gender = gender
+        self.age = age
         self.height = height
-        self.weight = weight
-        self.colour = colour
+        self.name = name
+    
+person_a = Person("Male", 26, "175cm", "Henry")
 
-    def print_colour(self):
-        print(self.colour)
-
-test = MrRoboto()
-test.print_colour()
-
-og_init = MrRoboto.__init__
-def updated_colour(self, *args, **kwargs):
-    og_init(self, *args, **kwargs)
-    self.colour = "Blue"
-
-MrRoboto.__init__ = updated_colour
-
-new_test = MrRoboto()
-new_test.print_colour()
+attr_name = "gender"
+asked_info = getattr(person_a, attr_name)
+print(asked_info)
