2ef28d6391b7bf24719a9bc4536e262cca9f7d45
heyhenry <henryyy3@hotmail.com>
2025-07-22 11:11:51 +0800

    Achievements: Updates structure of achievement images to be more dynamic.

--- pages/images/achievement_images.py
+++ pages/images/achievement_images.py
@@ -5,42 +5,46 @@ achievements_triple_stars = ctk.CTkImage(light_image=Image.open("img/achievement
 achievements_banner = ctk.CTkImage(light_image=Image.open("img/achievements/original_icons/achievement_banner.png"), dark_image=Image.open("img/achievements/original_icons/achievement_banner.png"), size=(96, 96))
 loading_achievement_icon = ctk.CTkImage(light_image=Image.open("img/achievements/original_icons/loading_achievement.png"), dark_image=Image.open("img/achievements/original_icons/loading_achievement.png"), size=(96, 96))
 
-locked_achievement_1_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_2_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)) 
-locked_achievement_3_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_4_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_5_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_6_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_7_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_8_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_9_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_10_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_11_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_12_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_13_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_14_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_15_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_16_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_17_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_18_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
-locked_achievement_19_icon = ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
+locked_achievements = {
+   1 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   2 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   3 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   4 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   5 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   6 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   7 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   8 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   9 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   10 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   11 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   12 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   13 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   14 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   15 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   16 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   17 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   18 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96)),
+   19 : ctk.CTkImage(light_image=Image.open("img/achievements/locked_version/first_day.png"), dark_image=Image.open("img/achievements/locked_version/first_day.png"), size=(96, 96))
+}
 
-unlocked_achievement_1_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_2_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)) 
-unlocked_achievement_3_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_4_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_5_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_6_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_7_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_8_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_9_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_10_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_11_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_12_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_13_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_14_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_15_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_16_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_17_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_18_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
-unlocked_achievement_19_icon = ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
+unlocked_achievements = {
+   1 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   2 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   3 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   4 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   5 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   6 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   7 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   8 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   9 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   10 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   11 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   12 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   13 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   14 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   15 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   16 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   17 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   18 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96)),
+   19 : ctk.CTkImage(light_image=Image.open("img/achievements/unlocked_version/first_day.png"), dark_image=Image.open("img/achievements/unlocked_version/first_day.png"), size=(96, 96))
+}
