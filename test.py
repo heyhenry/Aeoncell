efbae70d926313586db9afae5ed4a55cf21455dd
heyhenry <henryyy3@hotmail.com>
2025-07-25 09:38:16 +0800

    Test: Trial and error to find best way to retrieve, update and display latest achievement data for dashboard/profile_section.

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
-class Person():
-    def __init__(self, gender, age, height, name):
-        self.gender = gender
-        self.age = age
-        self.height = height
-        self.name = name
-   
-person_1_info = Person("Male", 26, "175cm", "Henry")
-person_2_info = Person("Male", 26, "175cm", "Jake")
-person_3_info = Person("Male", 26, "175cm", "Ray")
-person_4_info = Person("Male", 26, "175cm", "Henson")
-
-people = {
-    1: person_1_info,
-    2: person_2_info,
-    3: person_3_info,
-    4: person_4_info
+achievement_names = {
+    i : ""
+    for i in range(1, 5)
 }
+achievement_badges = {
+    i : "Badge Goes Here"
+    for i in range(1, 5)
+}
+achievement_unlock_dates = {
+    i : ""
+    for i in range(1, 5)
+}
+print(achievement_names)
+results = [(6, 'new_profile', 'Unlocked 25 Jul, 2025, 09:07 AM'), (1, 'first_day', 'Unlocked 25 Jul, 2025, 08:44 AM'), (2, 'first_drink', ''), (3, 'first_sleep', '')]
+for i in range(len(results)):
+    if results[i][2] == "":
+        continue
+    achievement_names[i+1] = results[i][1]
+    achievement_badges[i+1] = f"achievement: {results[i][0]} badge here"
+    achievement_unlock_dates[i+1] = results[i][2]
 
 for i in range(1, 5):
-    print(getattr(people[i], "name"))
+    print(f"==========[Badge Slot {i}]==========")
+    print(f"Achievement Name: {achievement_names[i]}")
+    print(f"Achievement Badge: {achievement_badges[i]}")
+    print(f"Achievement Unlock Date: {achievement_unlock_dates[i]}")
+    print("=====================================\n")
 
